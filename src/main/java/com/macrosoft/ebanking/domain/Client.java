68cbbc0959a8bbb41372d80bff03199f2c274976
SOULEYSANOGO <SOULEYSANOGO@CIEW15854>
2022-09-20 11:49:41 +0000

    Update tempalate

---
+++ src/main/java/com/macrosoft/ebanking/domain/Client.java
@@ -0,0 +1,14 @@
+package  com.macrosoft.ebanking;
+
+//@Entity
+public class Client {
+
+   /* @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)*/
+    private long id;
+
+    private String name;
+    private String email;
+
+    // standard getters and setters
+}
